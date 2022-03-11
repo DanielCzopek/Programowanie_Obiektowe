e79d6cae1fb488ad1ba7f7502e84bd8a830a4189
Daniel Czopek <Daniel.Czopek@microsoft.wsei.edu.pl>
2022-03-11 11:34:10 +0100

    dodano wiele

--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -6,46 +6,49 @@ namespace Lab_01
     {
         static void Main(string[] args)
         {
-            Ulamek ulamek = new Ulamek(1, 2);
+            Ulamek ulamek = new Ulamek(1, 4);
             Ulamek ulamek2 = new Ulamek(3, 4);
             Ulamek ulamek3 = ulamek + ulamek2;
+            Ulamek ulamek4 = ulamek3 - ulamek2;
+            Ulamek ulamek5 = ulamek4 * ulamek;
+            Ulamek ulamek6 = ulamek5 / ulamek2;
+
+            Console.WriteLine(ulamek.ToString());
+            Console.WriteLine(ulamek2.ToString());
+            Console.WriteLine(ulamek3.ToString());
+            Console.WriteLine(ulamek4.ToString());
+            Console.WriteLine(ulamek5.ToString());
+            Console.WriteLine(ulamek6.ToString());
         }
-    }
-    interface IEquatable
-    {
 
     }
-    interface IComparable : IEquatable
-    {
-
-    }
-    class Ulamek : IComparable
-
-
-
-    {
+    
+    
+     public class Ulamek : IEquatable<Ulamek>, IComparable<Ulamek>
+    { 
         private int licznik { get; }
 
         private int mianownik { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
 
         public Ulamek()
         {
 
         }
-
         public Ulamek(int licznik, int mianownik)
         {
-
+            this.licznik = licznik;
+            this.mianownik = mianownik;
         }
 
         public Ulamek(Ulamek ulamek)
         {
             this.licznik = ulamek.licznik;
         }
-        public override string ToString()
-        {
-            return "działa?";
-        }
+       
         public static Ulamek operator +(Ulamek a, Ulamek b)
         {
             return new Ulamek(a.licznik * b.mianownik + b.licznik + b.mianownik, a.mianownik * b.mianownik);
@@ -60,16 +63,34 @@ namespace Lab_01
         }
         public static Ulamek operator /(Ulamek a, Ulamek b)
         {
-            if (b.licznik == 0)
-            {
-                throw new DivideByZeroException();
-            }
+           if (b.licznik == 0)
+           {
+               throw new DivideByZeroException();
+           }
             return new Ulamek(a.licznik * b.mianownik, a.mianownik * b.licznik);
         }
-        public static Ulamek operator (int licznik, int mianownik)
+        public bool Equals(Ulamek other)
         {
-            Math.Round(licznik / mianownik);
-        }
+            if (other == null) return false;
+            if (other == this) return true;
 
+            return Object.Equals(this.licznik, other.licznik) && Object.Equals(this.mianownik, other.mianownik);
+        }
+        public int CompareTo(Ulamek other)
+        {
+            if (other == null) return +1;
+            if (other == this) return 0;
+
+            int diff = this.mianownik - other.mianownik;
+            if (diff > 0) return +1;
+            if (diff < 0) return -1;
+
+            return 0;
+        }
+        public override string ToString()
+        {
+            return licznik + "/" + mianownik;
+        }
+        
     }
 }
