666a6db52e4bd8ff4787028a9948fa3f048b618f
DanielCzopek <101318216+DanielCzopek@users.noreply.github.com>
2022-03-25 10:14:10 +0100

    Zadanie z labów 3

---
+++ Lab_3/Lab_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "text-123";
+            Print(a + "1");
+            Print("2");
+            Print("3");
+        }
+        public static void Print(string text)
+        {
+            Console.WriteLine("print: " + text);
+        }
+    }
+}
