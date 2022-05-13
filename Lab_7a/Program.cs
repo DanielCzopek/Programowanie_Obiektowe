7dbb3d49ea37db9e88210a5d2e3c9a05cce003b7
Daniel Czopek <Daniel.Czopek@microsoft.wsei.edu.pl>
2022-05-13 10:16:15 +0200

    Uzupełnienie do lab7

---
+++ Lab_7a/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var user = new User
+        {
+            Id = 1,
+            Name = "Jan Kowalski",
+            Age = 30,
+        };
+
+        string json = JsonSerializer.Serialize(user);
+
+        Console.WriteLine(json); // {"Id":1,"Name":"Jan Kowalski","Age":30}
+    }
+}
