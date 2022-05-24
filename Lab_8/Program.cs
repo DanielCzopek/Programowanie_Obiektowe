73430264c1aa0cbe3e7a4694931075371d00bb5e
Daniel Czopek <danielczopek102@gmail.com>
2022-05-24 19:53:32 +0200

    poprawiono laby 18 i coś tam zrobiono z domowego

--- Lab_8/Program.cs
+++ Lab_8/Program.cs
@@ -3,53 +3,36 @@ using System.Threading;
 
 namespace Lab_8
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			Thread thread = new Thread(() =>
-			{
-				for (int j = 1; j <= 10; ++j)
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread thread = new Thread(() =>
+            {
+                for (int i = 1; i <= 5; ++i)
+                {
+                    Console.WriteLine("Iteration1: " + i);
+                    Thread.Sleep(1000); // Zatrzymuje wątek na 1000ms czyli 1s,
+                                        // powoduje to że cały odpalając program treści w konsoli wyświetlają się stopniowo a nie wszystkie na raz
+                }
+            });
+            Thread thread2 = new Thread(() =>
+            {
+                for (int i = 1; i <= 5; ++i)
+                {
+                    Console.WriteLine("Iteration2: " + i);
+                    Thread.Sleep(1000);
+                }
+            });
+            thread.Start();
+            thread2.Start();
 
-				{
-					static bool czyPierwsza(int j)
-					{
-						for (int i = 2; i <= Math.Sqrt(j); i++)
-						{
-							if (j % i == 0)
-							{
-								return false;
-							}
-						}
-						return true;
-					}
+            Thread.Sleep(1000);  // Opóźnia główny wątek programu o 1 sekunde
 
-					if (czyPierwsza(j))
-					{
-						Console.WriteLine("Iteration1: " + j);
-					}
-					Thread.Sleep(1000); // Zatrzymuje wątek na 1000ms czyli 1s,
-										// powoduje to że cały odpalając program treści w konsoli wyświetlają się stopniowo a nie wszystkie na raz
-				}
-			});
-
-			//Thread thread2 = new Thread(() =>
-			//{
-			//	for (int i = 1; i<= 5; i++)
-			//             {
-			//                 Console.WriteLine("Interation2: " + i);
-			//		Thread.Sleep(1000);
-			//             }
-			//});
-
-			//thread2.Start();
-
-			thread.Start();
+            //  usypia główny wątek do momętu aż utworzony wątek nie zostanie usunięty
 
 
-			//Oba wątki wywołują się w tym samym czasie.
-
-			// Thread.Join() usypia główny wątek do momętu aż utworzony wątek nie zostanie usunięty
-		}
-	}
+            //Oba wątki wywołują się w tym samym czasie.
+        }
+    }
 }
