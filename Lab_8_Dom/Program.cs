8bf8e2e570f3f792f18c85842affab2b8ebdd662
Daniel Czopek <danielczopek102@gmail.com>
2022-05-24 21:23:50 +0200

    Zrobiłem tak że działa najpierw 1 potem 2

--- Lab_8_Dom/Program.cs
+++ Lab_8_Dom/Program.cs
@@ -8,8 +8,40 @@ namespace Lab_8_Dom
 
         public static void Main(string[] args)
         {
+            //W taki sposób działa na jednym wątku ideolo
+
+            //HashSet<int> primeNumbers = new HashSet<int>();
+            //bool looped = true;
+
+
+            //Thread thread1 = new Thread(() =>
+            //{
+            //    Console.WriteLine("Started");
+
+            //    for (int i = 1; looped; ++i)
+            //    {
+            //        if (czyPierwsza(i) == true)
+            //        {
+            //            primeNumbers.Add(i);
+            //            Console.WriteLine("Iteration1: " + i);
+
+            //        }
+            //    }
+            //    Console.WriteLine("Stopping");
+            //});
+
+
+            //thread1.Start();
+            //Thread.Sleep(10000);
+            //looped = false;
+            //thread1.Join();
+
+            //bool looped2 = true;
+
             HashSet<int> primeNumbers = new HashSet<int>();
             bool looped = true;
+
+
             Thread thread1 = new Thread(() =>
             {
                 Console.WriteLine("Started");
@@ -19,33 +51,58 @@ namespace Lab_8_Dom
                     if (czyPierwsza(i) == true)
                     {
                         primeNumbers.Add(i);
-                        Console.WriteLine("Iteration: " + i);
+                        Console.WriteLine("Iteration1: " + i);
 
                     }
                 }
+                Console.WriteLine("Stopping");
+            });
 
+            bool looped2 = true;
+
+            Thread thread2 = new Thread(() =>
+            {
+                Console.WriteLine("Started");
+
+                for (int i = 1; looped2; ++i)
+                {
+                    if (czyPierwsza(i) == true)
+                    {
+                        primeNumbers.Add(i);
+                        Console.WriteLine("Iteration2: " + i);
+
+                    }
+                }
                 Console.WriteLine("Stopping");
             });
 
             thread1.Start();
-            Thread.Sleep(10000);
+          Thread.Sleep(1000);
             looped = false;
             thread1.Join();
-
-            static bool czyPierwsza(int j)
-            {
-                for (int i = 2; i <= Math.Sqrt(j); i++)
-                {
-                    if (j % i == 0)
-                    {
-                        return false;
-                    }
-                }
-                return true;
-            }
-
+        
+            thread2.Start();
+            Thread.Sleep(1000);
+            looped2 = false;
+            thread2.Join();
         }
+        
+
+
+        static bool czyPierwsza(int j)
+        {
+            for (int i = 2; i <= Math.Sqrt(j); i++)
+            {
+                if (j % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
     }
 }
+
 
 
